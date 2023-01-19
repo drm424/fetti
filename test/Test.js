3286634d73db11c9730898a26f8b7f11e5725418
drm424 <drm424@lehigh.edu>
2023-01-18 19:09:21 -0500

    readme and non functional testing

--- test/Test.js
+++ test/Test.js
@@ -39,6 +39,7 @@ describe("Test", function () {
     it("Mints 10 USDC to msg.sender upon deployment", async function () {
       const { usdc, owner} = await loadFixture(deployTestTokenFixture);
       const ownerBalance = await usdc.balanceOf(owner.address);
+      
       expect(Number(ownerBalance)).to.equal(Number(10**7));
     });
 
@@ -48,17 +49,21 @@ describe("Test", function () {
 
     it("Owner can send 10 USDC to vault", async function () {
       const { usdc, token, vault, owner} = await loadFixture(deployTestTokenFixture);
+      
       const ownerBalance = 10**7;
       await usdc.connect(owner).approve(token.address, ownerBalance);
       await token.connect(owner).deposit(ownerBalance,owner.address);
+      
       expect(Number(await vault.totalAssets())).to.equal(Number(10**7));
     });
 
     it("Owner gets 10 FET upon deposit", async function () {
       const { usdc, token, vault, owner} = await loadFixture(deployTestTokenFixture);
+      
       const ownerBalance = 10**7;
       await usdc.connect(owner).approve(token.address, ownerBalance);
       await token.connect(owner).deposit(ownerBalance,owner.address);
+      
       expect(Number(await token.balanceOf(owner.address))).to.equal(Number(10**7));
     });
 
@@ -69,9 +74,11 @@ describe("Test", function () {
     it("Can widthdraw all FET", async function () {
       const { usdc, token, vault, owner} = await loadFixture(deployTestTokenFixture);
       const ownerBalance = 10**7;
+      
       await usdc.connect(owner).approve(token.address, ownerBalance);
       await token.connect(owner).deposit(ownerBalance,owner.address);
       await token.connect(owner).redeem(ownerBalance,owner.address,owner.address);
+      
       expect(Number(await token.balanceOf(owner.address))).to.equal(Number(0));
       expect(Number(await usdc.balanceOf(owner.address))).to.equal(Number(10**7));
     });
@@ -79,9 +86,11 @@ describe("Test", function () {
     it("Can widthdraw half of FET", async function () {
       const { usdc, token, vault, owner} = await loadFixture(deployTestTokenFixture);
       const ownerBalance = 10**7;
+      
       await usdc.connect(owner).approve(token.address, ownerBalance);
       await token.connect(owner).deposit(ownerBalance,owner.address);
       await token.connect(owner).redeem((ownerBalance/2),owner.address,owner.address);
+      
       expect(Number(await token.balanceOf(owner.address))).to.equal(Number((10**6)*5));
       expect(Number(await usdc.balanceOf(owner.address))).to.equal(Number((10**6)*5));
     });
@@ -92,18 +101,35 @@ describe("Test", function () {
 
     it("Can add a holding to the vault", async function () {
       const { usdc, token, vault, owner, holding} = await loadFixture(deployTestTokenFixture);
+      
       expect(await vault.getHoldingAddress(1)).to.equal(holding.address);
     });    
+
+    it("Gives correct max redeem/widthdraw values taking non-liquid positions into account", async function(){
+      const { usdc, token, vault, owner, holding} = await loadFixture(deployTestTokenFixture);
+      const ownerBalance = 10**7;
+
+      await usdc.connect(owner).approve(token.address, ownerBalance);
+      await token.connect(owner).deposit(ownerBalance,owner.address);
+      await token.connect(owner).redeem(ownerBalance,owner.address,owner.address);
+      await vault.connect(owner).addToHolding(5*10**6, 1);
+      
+      expect(Number(await vault.totalAssets())).to.equal(Number(10**7));
+      expect(Number(await vault.totalUsdcInVault())).to.equal(Number(5*10**6));
+      expect(Number(await token.maxRedeem(owner.address))).to.equal(Number(5*10**6));
+
+    });
     
     it("Can send usdc to a holding", async function () {
       const { usdc, token, vault, owner, holding} = await loadFixture(deployTestTokenFixture);
       const ownerBalance = 10**7;
+      
       await usdc.connect(owner).approve(token.address, ownerBalance);
       await token.connect(owner).deposit(ownerBalance,owner.address);
       await vault.connect(owner).addToHolding(5*(10**6),1);
+      
       expect(Number(await usdc.balanceOf(holding.address))).to.equal(Number(5*(10**6)));
       expect(Number(await vault.totalAssets())).to.equal(Number(10**7));
     });
-
   });
 });
