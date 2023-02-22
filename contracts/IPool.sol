76a2202efbdb3c50b253dbef6591b822564e4a2e
drm424 <drm424@lehigh.edu>
2023-02-22 18:31:39 -0500

    cleaned up the place, what you think?

--- contracts/IPool.sol
+++ contracts/IPool.sol
@@ -5,23 +5,26 @@ interface IPool{
     
     function totalLoanedOut() external view returns(uint256 amount);
 
-    function depositColateral(address receiver_, uint256 amount_) external returns(uint256 loanId);
+    function depositColateral(address receiver_, uint256 amount_) external returns(uint256);
 
-    function depositColateralEth() external payable returns(uint256 loanId);
+    function depositColateralEth() external payable returns(uint256);
 
-    function addColateral(uint256 loanId_, uint256 amount) external returns(uint256 totalColateral);
+    function addColateral(uint256 loanId_, uint256 amount) external returns(uint256);
 
-    function addColateralEth(uint256 loanId_) external payable returns(uint256 totalColateral);
-
-    function totalColateral(uint256 loanId_) external returns(uint256 amount);
+    function addColateralEth(uint256 loanId_) external payable returns(uint256);
 
     function widthdrawColateral(address receiver_, uint256 loanId_) external returns(uint256);
 
     function widthdrawColateralEth(address payable receiver_, uint256 loanId_) external payable returns(uint256);
 
-    function borrow(uint256 loanId_, uint256 amount_, address payable sendTo_) external returns(uint256 amount);
+    function borrow(uint256 loanId_, uint256 amount_, address payable sendTo_) external returns(uint256);
 
-    function maxBorrow(uint256 loanId_, address sendTo_) external returns(uint256 amount);
+    function repayLoan(uint256 loanId_, uint256 amount_) external returns(uint256);
 
-    function poke(address sendRewards_) external returns(uint256 amount);
+    function liquidate(uint256 loanId_ ) external; 
+
+    function close(uint256 loanId_) external;
+
+    function totalColateral(uint256 loanId_) external returns(uint256);
+
 }
--- contracts/ethPool.sol
+++ contracts/ethPool.sol
@@ -11,19 +11,20 @@ contract ethPool is IPool, ERC721{
     event LoanOpen(address mintor, uint256 tokenId, uint256 amount);
     event AddedColateral(uint256 tokenId, uint256 amount);
     event LoanClose(address receiver, uint256 tokenId, uint256 amount);
-
     event SentLoan(uint256 tokenId, uint256 amount);
+    event RepaidLoan(uint256 tokenId, address payer, uint256 amountRepaid, uint256 outstandingLoanValue);
+    event Liquidation(uint tokenId, uint256 amountToVault, uint256 amountToBorrower, uint256 amountToFund);
 
+    //create mapping(address=>uint256) for percentage of accrrued rewards for each associated address
+    //also need an array of address that will be paid to iterate through mapping to pay rewards
     struct Loan{
         uint256 id;
         uint256 depositedEth;
         uint256 borrowedUsdc;
         uint256 unlockTime;
+        uint256 maxBorrowedUsdc;
         uint256 maxHealthFactor;
         uint256 liquidationPenalty;
-        uint256 borrowerSplit;
-        uint256 lenderSplit;
-        uint256 projectSplit;
     }
 
     mapping(uint256=>Loan) private _outstandingLoans;
@@ -40,52 +41,35 @@ contract ethPool is IPool, ERC721{
         _gov = msg.sender;
         _usdc = IERC20(usdc_);
         _loaner = ILoaner(loaner_);
-        _count = 0;
         _vault = vault_;
+        _count = 0;
+        _currLoanedOut = 0;
     }
 
-    function setLoanerId(uint256 poolId_) external returns(uint256 id){
+    function setLoanerId(uint256 poolId_) external returns(uint256){
         require(msg.sender==_gov,"only gov!!!");
         _loanerId = poolId_;
         return _loanerId;
     }
 
     //add global var and track loaned out amounts with each borrow and repayment
-    function totalLoanedOut() external view returns(uint256 amount){
+    function totalLoanedOut() external view returns(uint256){
         return _currLoanedOut;
-    }
-
-
-    //not needed for eth
-    function depositColateral(address receiver_, uint256 amount_) external pure returns(uint256 loanId){
-        require(0==1, "This is an eth loan");
-        return 0;
-    }
-
-    //not needed for eth
-    function addColateral(uint256 loanId_, uint256 amount) external pure returns(uint256){
-        require(0==1, "This is an eth loan");
-        return 0;
-    }
-
-    //not needed for eth
-    function widthdrawColateral(address receiver_, uint256 loanId_) external pure returns(uint256){
-        require(0==1, "This is an eth loan");
-        return 0;
     }
 
     //eventually change block.timestamp to chainlink oracle
     //after get loan functionality done swap to sfxseth
     function depositColateralEth() external payable returns(uint256){
         require(msg.value>0,"Must deposit eth!!!");
+        //may want to think about taking this line out
         require(balanceOf(msg.sender)==0, "can only have one loan per address");
         _count+=1;
-        uint256 countLocal = _count;
+        //change 1 to an inputted lock time and save a widthdraw fee asscoiated with that lock
         uint256 unlockTime = block.timestamp + 1;
-        _outstandingLoans[countLocal] = Loan(countLocal,msg.value,0,unlockTime,80,10,45,45,10);
-        _safeMint(msg.sender, countLocal);
-        emit LoanOpen(msg.sender,countLocal,msg.value);
-        return countLocal;
+        _outstandingLoans[_count] = Loan(_count,msg.value,0,unlockTime,70,75,8);
+        _safeMint(msg.sender, _count);
+        emit LoanOpen(msg.sender,_count,msg.value);
+        return _count;
     }
 
     function addColateralEth(uint256 loanId_) external payable returns(uint256){      
@@ -104,21 +88,15 @@ contract ethPool is IPool, ERC721{
         require(_outstandingLoans[loanId_].borrowedUsdc==0,"Must repay entire loan before removing colateral");
         require(msg.sender==ownerOf(loanId_),"Must be the owner of the loan");
         uint256 amount = _outstandingLoans[loanId_].depositedEth + 0;
-        delete _outstandingLoans[loanId_];
-        _burn(loanId_);
+        _closeLoan(loanId_);
         receiver_.transfer(amount);   
         emit LoanClose(receiver_, loanId_, amount);     
         return amount;
     }
 
-    function totalColateral(uint256 loanId_) public view returns(uint256){
-        require(_exists(loanId_),"loanId must be a open loan");
-        return _outstandingLoans[loanId_].depositedEth;
-    }
-
-    function borrow(uint256 loanId_, uint256 amount_, address payable sendTo_) external returns(uint256 amount){
+    function borrow(uint256 loanId_, uint256 amount_, address payable sendTo_) external returns(uint256){
         require(_exists(loanId_),"Loan doesnt exist");
-        require(getNewHealth(loanId_, amount_)<(7*1e5), "Requesting too much usdc");
+        require(getNewHealth(loanId_, amount_)<(_outstandingLoans[loanId_].maxBorrowedUsdc*1e4), "Requesting too much usdc");
         require(msg.sender==ownerOf(loanId_),"must be owner");
         require(_loaner.poolFreeUsdc(1)!=0,"loaner does not have enough usdc to cover your amount");
         _currLoanedOut+=amount_;
@@ -126,6 +104,37 @@ contract ethPool is IPool, ERC721{
         _loaner.sendLoan(sendTo_,_loanerId,amount_);
         emit SentLoan(loanId_, amount_);
         return amount_;
+    }
+
+    //not exactly sure of the correct order of these transactions
+    function repayLoan(uint256 loanId_, uint256 amount_) external returns(uint256){
+        require(_exists(loanId_),"loanId must be a open loan");
+        require(amount_<=_outstandingLoans[loanId_].borrowedUsdc,"amount must be greater than 0 and less than your loaned out amount");
+        _usdc.transferFrom(msg.sender,_vault,amount_);
+        _currLoanedOut-=amount_;
+        _outstandingLoans[loanId_].borrowedUsdc-=amount_;
+        emit RepaidLoan(loanId_, msg.sender, amount_, _outstandingLoans[loanId_].borrowedUsdc);
+        return _outstandingLoans[loanId_].borrowedUsdc;
+    }
+
+    //needs frax funcionality before completion
+    //probably need a function to split rewards 
+    function liquidate(uint256 loanId_) external{
+         
+    }
+
+    //needs frax funcionality before completion
+    function close(uint256 loanId_) external{
+
+    }
+
+    //function that splits the rewards for the loan
+    //since the loan is deleted before rewards are sent probably take in the mapping(address=>uint256) split
+    //function splitRewards(loanId_) external;
+
+    function totalColateral(uint256 loanId_) public view returns(uint256){
+        require(_exists(loanId_),"loanId must be a open loan");
+        return _outstandingLoans[loanId_].depositedEth;
     }
 
     function totalBorrow(uint256 loanId_) public view returns(uint256){
@@ -145,31 +154,36 @@ contract ethPool is IPool, ERC721{
         return healthFactor1e6;
     }
 
-    //not exactly sure of the correct order of these transactions
-    function repayLoan(uint256 loanId_, uint256 amount_) public returns(uint256){
-        require(_exists(loanId_),"loanId must be a open loan");
-        require((amount_>0)&&(amount_<=_outstandingLoans[loanId_].borrowedUsdc),"amount must be greater than 0 and less than your loaned out amount");
-        _usdc.transferFrom(msg.sender,_vault,amount_);
-        _currLoanedOut-=amount_;
-        _outstandingLoans[loanId_].borrowedUsdc-=amount_;
-        return _outstandingLoans[loanId_].borrowedUsdc;
-    }
-
-    function maxBorrow(uint256 loanId_, address sendTo_) external pure returns(uint256 amount){
-        return 0;
-    }
-
-    function poke(address sendRewards_) external pure returns(uint256 amount){
-        return 0;
-    }
-
     //eventually call chainlink price oracle here
-    function currEthPrice() public pure returns(uint256 price){
+    function currEthPrice() public pure returns(uint256){
         return 1000;
     }
 
     //purely for testing
     function exists(uint256 tokenId) public view returns (bool) {
         return _ownerOf(tokenId) != address(0);
+    }
+
+    function _closeLoan(uint256 loanId_) private{
+        delete _outstandingLoans[loanId_];
+        _burn(loanId_);
+    }
+
+    //not needed for eth
+    function depositColateral(address receiver_, uint256 amount_) external pure returns(uint256){
+        require(0==1, "This is an eth loan");
+        return 0;
+    }
+
+    //not needed for eth
+    function addColateral(uint256 loanId_, uint256 amount) external pure returns(uint256){
+        require(0==1, "This is an eth loan");
+        return 0;
+    }
+
+    //not needed for eth
+    function widthdrawColateral(address receiver_, uint256 loanId_) external pure returns(uint256){
+        require(0==1, "This is an eth loan");
+        return 0;
     }
 }
